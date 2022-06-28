a096a716d2255d2ba3420ef10f5afbe9a2646020
Pierre-Loïc Pichon <pierreloic.pichon@gmail.com>
2022-06-28 18:49:10 +0200

    Add more explicit console outputs

--- run.rb
+++ run.rb
@@ -9,22 +9,30 @@ require 'nokogiri'
 require 'open-uri'
 
 threshold = (ENV['THRESHOLD'] || 17).to_i
-draw_days = (ENV['DRAW_DAYS'] || 'mardi,vendredi')
+draw_days = (ENV['DRAW_DAYS'] || 'mardi,vendredi').split(',')
+today = { 2 => 'mardi', 5 => 'vendredi' }[Time.now.wday]
 
-# Stop the script if today is not a desired draw day
-return unless draw_days.split(',').include?({ 2 => 'mardi', 5 => 'vendredi' }[Time.now.wday])
+print 'Is today a desired draw day? '
+if draw_days.include? today
+  puts "✓ (#{today})"
+else
+  puts "✕ (#{today})"
+  return
+end
 
 base_url = 'https://www.fdj.fr/jeux-de-tirage/euromillions-my-million/resultats'
 response = URI.parse(base_url).read
 html = Nokogiri::HTML(response)
 
 amount = html.css('.result-full__announce-gainNum').text.strip.to_i
-next_draw_day = html.css('.result-full__announce-date').text.downcase.split.first
 
-puts "#{amount}M€ (vs #{threshold}M€)"
-puts "#{next_draw_day} (vs #{draw_days})"
-
-return unless amount >= threshold
+print "Has the jackpot reached #{threshold}M€? "
+if amount >= threshold
+  puts "✓ (#{amount}M€)"
+else
+  puts "✕ (#{amount}M€)"
+  return
+end
 
 print "Sending email to #{ENV['MAILER_USER_NAME']}... "
 
@@ -44,7 +52,7 @@ end
 mail = Mail.new do
   from    ENV['MAILER_USER_NAME']
   to      ENV['MAILER_USER_NAME']
-  subject "Euromillion: #{amount}M€ ce #{next_draw_day}"
+  subject "Euromillion: #{amount}M€ ce #{today}"
 end
 
 mail.deliver!
